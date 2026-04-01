c5f9cc6cd78682b327e88cdfbfcfd921c25e02ac
JivanKesan <jkesan28@gmail.com>
2026-04-01 16:59:16 -0400

    removed unused code

---
+++ raw/build/CMakeFiles/robot.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/Jivan/Desktop/380Robot/raw/src/control.cpp" "CMakeFiles/robot.dir/src/control.cpp.o" "gcc" "CMakeFiles/robot.dir/src/control.cpp.o.d"
+  "/Users/Jivan/Desktop/380Robot/raw/src/fsm.cpp" "CMakeFiles/robot.dir/src/fsm.cpp.o" "gcc" "CMakeFiles/robot.dir/src/fsm.cpp.o.d"
+  "/Users/Jivan/Desktop/380Robot/raw/src/main.cpp" "CMakeFiles/robot.dir/src/main.cpp.o" "gcc" "CMakeFiles/robot.dir/src/main.cpp.o.d"
+  "/Users/Jivan/Desktop/380Robot/raw/src/serial.cpp" "CMakeFiles/robot.dir/src/serial.cpp.o" "gcc" "CMakeFiles/robot.dir/src/serial.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ raw/build/CMakeFiles/robot.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/robot.dir/src/control.cpp.o"
+  "CMakeFiles/robot.dir/src/control.cpp.o.d"
+  "CMakeFiles/robot.dir/src/fsm.cpp.o"
+  "CMakeFiles/robot.dir/src/fsm.cpp.o.d"
+  "CMakeFiles/robot.dir/src/main.cpp.o"
+  "CMakeFiles/robot.dir/src/main.cpp.o.d"
+  "CMakeFiles/robot.dir/src/serial.cpp.o"
+  "CMakeFiles/robot.dir/src/serial.cpp.o.d"
+  "robot"
+  "robot.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/robot.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
